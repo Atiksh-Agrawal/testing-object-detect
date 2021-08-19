46580dc9c2a593a73c96306dc1745821c2cc1b2c
Atiksh-Agrawal <74902465+Atiksh-Agrawal@users.noreply.github.com>
2021-08-19 15:38:50 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,47 @@
+status = "";
+
+var objects = [];
+
+function preload(){
+    img = loadImage("dog_cat.jpg");
+}
+
+function setup(){
+    canvas = createCanvas(700,480);
+    canvas.center();
+
+    objectDetector = ml5.objectDetector("cocossd",modelLoaded);
+    document.getElementById("status").innerHTML = "Status : Detecting object";
+}
+
+function draw(){
+    image(img ,0,0,700,480);
+
+if(status != ""){
+  for(i=0 ; i<objects.length ; i++){
+    document.getElementById("status").innerHTML = "Status : Object detected !" ;
+    percent = floor(objects[i].confidence * 100);
+    fill("red");
+    stroke("red");
+    text(objects[i].label  +" "+percent + "%", objects[i].x, objects[i].y);
+    noFill();
+    rect(objects[i].x ,objects[i].y ,objects[i].width ,objects[i].height);
+  }
+}
+}
+
+function modelLoaded(){
+    console.log("CocoSsd is initialized");
+    status = true;
+    objectDetector.detect(img,gotResult);
+}
+
+function gotResult(error,results){
+   if(error){
+      console.error(error);
+}
+   else{
+       console.log(results);
+       objects = results;
+}
+}
